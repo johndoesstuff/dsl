1efcb9110ac2fb8c6241cdde1c7b6e2329ddc6ca
johndoesstuff <johndoesstuff03@gmail.com>
2025-02-07 13:52:44 -0500

    progress

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
-#include <time.h>
+#include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_EVENTS 256
+
+typedef struct {
+	char event_title[MAX_LINE_LENGTH];
+	long long event_time;
+} Event;
+
+long long current_time_ms() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (long long)(tv.tv_sec) * 1000 + (tv.tv_usec / 1000);
+}
 
 int main(int argc, char *argv[]) {
+	Event *events = malloc(MAX_EVENTS * sizeof(Event));
 	const char *home = getenv("HOME");
 	if (!home) {
 		fprintf(stderr, "Error: HOME environment variable not set.\n");
@@ -27,6 +43,51 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	char line[MAX_LINE_LENGTH];
+	int i = 0;
+	while (fgets(line, sizeof(line), file)) {
+		line[strcspn(line, "\n")] = '\0';
+		if (i%2 == 0) {
+			strncpy(events[i/2].event_title, line, MAX_LINE_LENGTH);
+		} else {
+			long long timeMS = 0LL;
+			sscanf(line, "%lld", &timeMS);
+			events[i/2].event_time = timeMS;
+		}
+		i++;
+	}
 
-	printf("Days Since Last\n");
+	if (argc > 1) {
+		if (strcmp(argv[1], "-add") == 0) {
+			if (argc < 3) {
+				perror("Please include the name of the thing to track");
+				return 1;
+			}
+			fclose(file);
+
+			file = fopen(filepath, "a");
+			if (!file) {
+				perror("Error opening file to append");
+				return 1;
+			}
+
+			fprintf(file, "%s\n", argv[2]);
+			fprintf(file, "%lld\n", current_time_ms());
+		}
+		if (strcmp(argv[1], "-clearall") == 0) {
+			file = fopen(filepath, "w");
+			fclose(file);
+		}
+	} else {
+		printf("Days Since Last\n");
+		rewind(file);
+
+		char line[MAX_LINE_LENGTH];
+		while (fgets(line, sizeof(line), file)) {
+			printf("%s", line);
+		}
+
+		fclose(file);
+		return 0;
+	}
 }
